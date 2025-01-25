d7a5cc9f6f9c794e87b216711754c1bd77a859ba
Derek Ruize <derekruiz@usf.edu>
2025-01-25 18:14:40 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+//Task 1
+let products = ["Airpods", "Ipad", "Iphone", "Macbook", "AirTag"]; //Products 
+products.push("Phone Case"); //Adding product to the Array
+
+let removedProduct = products.pop(); //Removing Last Product 
+
+console.log(products);
