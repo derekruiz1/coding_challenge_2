d624c7db16b006655b17b7ff44df0887e6fa8f24
Derek Ruize <derekruiz@usf.edu>
2025-01-25 21:38:51 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -5,3 +5,11 @@ products.push("Phone Case"); //Adding product to the Array
 let removedProduct = products.pop(); //Removing Last Product 
 
 console.log(products);
+
+//Task 2
+let scores = [95,78,47,67,55]; //Test Scores
+scores[1] = 85; // Score Update
+let average = scores.reduce((sum, scores) => (sum + scores))/scores.length; //Score Average
+
+console.log(scores);
+console.log(average);
